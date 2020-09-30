e463dc6d0bf141f2ba175fc98620ff169eab4328
Edwin Cruz <edwincruz255@gmail.com>
2020-09-29 20:17:38 -0600

    moved userform to signin route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import UserForm from './components/UserForm'
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import { useMediaQuery } from "@material-ui/core";
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: "light",
+          primary: {
+            main: "#2196f3"
+          }
+        }
+      }),
+    [prefersDarkMode]
+  );
   return (
-    <div className="App">
-      <UserForm></UserForm>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/signup" exact component={UserForm} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
