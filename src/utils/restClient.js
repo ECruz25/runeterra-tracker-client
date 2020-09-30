956700bf549302fd0f674e67fb4dd1eb79043b7f
Edwin Cruz <edwincruz255@gmail.com>
2020-09-30 16:36:46 -0600

    add services route

--- src/utils/restClient.js
+++ src/utils/restClient.js
@@ -1,8 +1,11 @@
 const getUrl = () => {
-  if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+  if (
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+  ) {
     return "https://localhost:44311/api";
   }
-  return "https://localhost:44311/api";
-}
+  return "https://runeterra-tracker-services.appspot.com/api";
+};
 
 export { getUrl };
